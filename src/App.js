ff20b6e04df23cac66f6dc10f4ddfb167d299309
Mars Lee <jlee10@brockport.edu>
2019-07-10 17:16:38 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items:[],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount () { // attach the data we get from the API to the state
+    fetch('http://jsonplaceholder.typicode.com/users') // grabbing this request
+    .then(res => { // res is response. now we do a callback method
+      return res.json(); // once we make a request, we make a reponse. take reponse, turn it into json, then go to data
+    })
+    .then(data => {; // array that has objects inside each indices, each object has key-value pairs
+      console.log(data); 
+      this.setState({
+        isLoaded: true, // change isLoaded to true before data is populated
+        items: data,
+      });
+    });
+  }
+
+  render() { //object deconstruction
+    var { isLoaded , items} = this.state // shorthand so don't need 
+    if (!isLoaded) { // if not loaded
+      return <div>Loading...</div> // Like load bar and or spinning circle cursor
+    } else { // map applies a function to each element in an array and returns a new array
+      return (
+        <div className="App">
+            <div className="Names">
+              <ul>
+                {items.map(el => {
+                  return (
+                    <li key={el.id}>
+                      Name: {el.name} | UserName: {el.username} | {' '} <a href={`https://${el.website}`}> Website</a>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+        </div>
+      )
+    } 
+  }
 }
 
 export default App;
